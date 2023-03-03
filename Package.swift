912c36f4d1a2c8e0d15c9d4bf7727bc842824ca6
Brian Henry <BrianHenryIE@gmail.com>
2023-03-02 23:23:52 -0800

    Remove iOS

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
         name: "BHSwiftOSLogStream",
         platforms: [
-            .iOS(.v12), .macOS(.v13)
+            .macOS(.v13)
         ],
         products: [
             // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -18,7 +18,6 @@ let package = Package(
         dependencies: [
             // Dependencies declare other packages that this package depends on.
             // .package(url: /* package url */, from: "1.0.0"),
-            //
             .package(
                     url: "https://github.com/apple/swift-collections.git",
                     .upToNextMajor(from: "1.0.0")
